81360c0141b6adc69246d03ccddd84b56283a720
Deepak <55588916+Deepakdekisugi@users.noreply.github.com>
2023-06-29 23:37:06 +0530

    Completed bitboards updatation

--- bitboards.c
+++ bitboards.c
@@ -1,7 +1,13 @@
 #include "stdio.h"
 #include "defs.h"
 
-void PrintBitBoards(U64 bb) {
+
+const int BitTable[64] = {
+    63, 30, 3, 32, 25, 41, 22, 33, 15, 50, 42, 13, 11, 53, 19, 34, 61, 29, 2,
+    
+};
+
+void PrintBitBoard(U64 bb) {
 
     U64 shiftMe = 1ULL;
 
@@ -15,6 +21,11 @@ void PrintBitBoards(U64 bb) {
         for(file = FILE_A; file <= FILE_H; ++file){
             sq = FR2SQ(file, rank);
             sq64 = SQ64(sq);
+
+            if((shiftMe << sq64) & bb)
+                printf("X");
+            else
+                printf("-");
         }
         printf("\n");
     }
