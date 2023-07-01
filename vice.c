d30d990f7942b4f47242d7a58903e905f8d4c10f
Deepak <55588916+Deepakdekisugi@users.noreply.github.com>
2023-07-01 20:46:32 +0530

    Printed bitboard

--- vice.c
+++ vice.c
@@ -4,7 +4,14 @@
 int main(){
 
     AllInit();
+    int index = 0;
+    U64 playBitBoard = 0ULL;
 
+    for (index = 0; index < 64; index++){
+        printf("Index : %d", index);
+        PrintBitBoard (SetMask[index]);
+        printf("\n");
+    }
    
 
     return 0;
