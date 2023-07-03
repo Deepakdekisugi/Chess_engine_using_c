e86c46901a5cd37bff848f77189c532ca2f88502
Deepak <55588916+Deepakdekisugi@users.noreply.github.com>
2023-07-03 23:50:53 +0530

    Update board.c

--- board.c
+++ board.c
@@ -8,5 +8,32 @@ void ResetBoard(S_BOARD *pos) {
 
     int index = 0;
 
+    for (index = 0; index < BRD_SQ_NUM; ++index){
+        pos -> pieces[index] = OFFBOARD;
+    }
+
+    for ( index = 0; index < 64; ++index) {
+        pos -> pieces[SQ120(index)] = EMPTY
+    }
+
+    for (index = 0; index < 3; ++index) {
+        pos -> bigPce[index] =0;
+        pos -> majPce[index] =0;
+        pos -> minPce[index] =0;
+        pos -> pawns[index] = 0ULL;
+    }
+    for (index = 0; index < 13; ++index) {
+        pos -> pceNum[index] = 0;
+    }
+
+    pos -> side = BOTH;
+    pos -> enPas = NO_SQ;
+    pos -> fiftyMove = 0;
+
+    pos -> ply = 0;
+    pos hisPly = 0;
     
+    pos -> castelPerm = 0;
+
+    pos -> posKey = 0ULL;
 }
