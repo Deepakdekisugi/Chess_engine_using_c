3d68d14c61036c8a79f56d69c6fcf99e347063e7
Deepakdekisugi <dk376907@gmail.com>
2023-11-21 11:36:24 +0530

    added few functions

--- polykeys.h
+++ polykeys.h
@@ -2,9 +2,9 @@
 #define POLYKEYS_H
 
 #ifdef _MSC_VER
-#  define U64_POLY(u) (u##ui64)
+#define U64_POLY(u) (u##ui64)
 #else
-#  define U64_POLY(u) (u##ULL)
+#define U64_POLY(u) (u##ULL)
 #endif
 
 extern const U64 Random64Poly[781];
